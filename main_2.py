49a090a41f428e5b1fac527e01dac8d4cdde103f
amalie-kjaer <amalieamkjaer@gmail.com>
2023-07-19 00:50:46 +0200

    add gpu option to main script

--- main_2.py
+++ main_2.py
@@ -1,5 +1,6 @@
 import argparse
 from trainer import train_model, test_model
+import torch
 
 def main(args):
     if args.mode == "train":
@@ -10,6 +11,8 @@ def main(args):
         pass
 
 if __name__=="__main__":
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--mode", choices=["train", "test", "train_test"], required=True)
     args = parser.parse_args()
